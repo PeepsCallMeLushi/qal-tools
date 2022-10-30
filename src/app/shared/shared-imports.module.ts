1f1d1acbbb506622fc82bbd6edddb18df2e7be46
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2022-10-30 18:07:43 +0000

    Credits, Fix to card list, loading dialog

--- src/app/shared/shared-imports.module.ts
+++ src/app/shared/shared-imports.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
@@ -17,10 +20,13 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
     ClipboardModule,
     MatAutocompleteModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatProgressSpinnerModule,
     MatTableModule,
+    MatTabsModule
   ]
 })
 export class SharedImportsModule { }
