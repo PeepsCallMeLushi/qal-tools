1fece5c1800ea81c15e016d37358dbd010800b1b
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2022-11-13 21:46:18 +0000

    Creating the nikke data set calculator, WIP!

---
+++ src/app/nikke/data-set-calculator/data-set-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'data-set-calculator',
+  templateUrl: './data-set-calculator.component.html',
+  styleUrls: ['./data-set-calculator.component.scss']
+})
+export class DataSetCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/nikke/nikke.module.ts
+++ src/app/nikke/nikke.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataSetCalculatorComponent } from './data-set-calculator/data-set-calculator.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
-
+const routes: Routes = [
+  {
+    path: 'data-set-calculator',
+    component: DataSetCalculatorComponent,
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DataSetCalculatorComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes),
   ]
 })
 export class NikkeModule { }
