27d58c2fdb86224f1e58a3b5f2a19cab95e36d02
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2022-11-13 19:16:09 +0000

    Defaulting to the MTG tool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'mtg-tool'
+  },
   { 
     path: 'mtg-tool',
     loadChildren: () => import('./mtg-tool/mtg-tool.module').then(m => m.MtgToolModule)
   },
-  
+
 ];
 
 @NgModule({
