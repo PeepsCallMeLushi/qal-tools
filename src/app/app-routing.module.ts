3d135e089bb90793776836868ab25bc37874578a
PeepsCallMeLushi <41750748+PeepsCallMeLushi@users.noreply.github.com>
2022-11-13 19:07:11 +0000

    Refactor to include routing and other tools

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { 
+    path: 'mtg-tool',
+    loadChildren: () => import('./mtg-tool/mtg-tool.module').then(m => m.MtgToolModule)
+  },
+  
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
